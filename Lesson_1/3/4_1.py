5310a259b232fa6c2cde5694ba73a3d99af71328
Shipovmax <shipov.max@icloud.com>
2025-10-26 14:04:30 +0300

    Выполнил

--- Lesson_1/3/4_1.py
+++ Lesson_1/3/4_1.py
@@ -5,7 +5,9 @@ def count_tiles(depth, length, width=0):
         width = length
 
     boot = length * width
-    l_size = 2 * ()
+    l_size = 2 * (depth * length)
+    w_size = 2 * (depth * width)
+    result = boot + l_size + w_size
 
     return result
 
