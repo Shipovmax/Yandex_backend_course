5ada726b92742afe582c6e178168024c556757d0
Shipovmax <shipov.max@icloud.com>
2025-10-28 00:14:12 +0300

    Выполнил

--- Lesson_1/4/2_2.py
+++ Lesson_1/4/2_2.py
@@ -4,9 +4,8 @@ def timedelta_days(datetime_str_1, datetime_str_2):
     
     datetime_1 = datetime.strptime(datetime_str_1, '%Y/%m/%d %H:%M:%S')
     datetime_2 = datetime.strptime(datetime_str_2, '%Y/%m/%d %H:%M:%S')
-    
-    
-    return
+    delta = datetime_2 - datetime_1
+    return abs(delta.days)
 
 difference = timedelta_days('2019/05/10 00:00:00', '2019/10/04 00:00:00')
 
